8c4b9e03421c90b311b9d8be2e2714d19a170886
Kasak0123 <kasak.yadav20@gmail.com>
2026-02-24 11:40:18 +0530

    Create multiplication_table.py

---
+++ multiplication_table.py
@@ -0,0 +1,25 @@
+# multiplication_table.py
+
+# Read input from standard input
+N = int(input())
+
+# Initialize counter
+i = 1
+
+# Print multiplication table using while loop
+while i <= 10:
+    print(f"{N} x {i} = {N * i}")
+    i += 1
+## output
+PS C:\Users\hp\OneDrive\Desktop\python_coding_problems> python multiplication_table.py
+5
+5 x 1 = 5
+5 x 2 = 10
+5 x 3 = 15
+5 x 4 = 20
+5 x 5 = 25
+5 x 6 = 30
+5 x 7 = 35
+5 x 8 = 40
+5 x 9 = 45
+5 x 10 = 50
