d32da222d3231190e82d2310704d978e42dd3cb8
Kasak0123 <kasak.yadav20@gmail.com>
2026-02-24 11:56:50 +0530

    Create star_pattern.py

---
+++ star_pattern.py
@@ -0,0 +1,16 @@
+# star_pattern.py
+
+# Read input from standard input
+N = int(input())
+
+# Print star pattern using for loop
+for i in range(1, N + 1):
+    print("*" * i)
+## output
+PS C:\Users\hp\OneDrive\Desktop\python_coding_problems> python star_pattern.py
+5
+*
+**
+***
+****
+*****
