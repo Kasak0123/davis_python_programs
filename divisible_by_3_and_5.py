689dbcd8dd7fc5ecb997958918ad2272557bf5dd
Kasak0123 <kasak.yadav20@gmail.com>
2026-02-24 18:56:53 +0530

    Create divisible_by_3_and_5.py

---
+++ divisible_by_3_and_5.py
@@ -0,0 +1,13 @@
+# Read input from standard input
+num = int(input())
+
+# Check divisibility
+if num % 15 == 0:
+    print("Divisible by both 3 and 5")
+else:
+    print("Not divisible by both 3 and 5")
+
+## output
+PS C:\Users\hp\OneDrive\Desktop\python_coding_problems> python divisible_by_3_and_5.py
+30
+Divisible by both 3 and 5
