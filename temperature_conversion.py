7ca78120bb34685fddc5669a484235c2a7a37a0f
Kasak0123 <kasak.yadav20@gmail.com>
2026-02-24 18:35:53 +0530

    Create temperature_conversion.py

---
+++ temperature_conversion.py
@@ -0,0 +1,15 @@
+# celsius_to_fahrenheit.py
+
+# Read input from standard input
+celsius = float(input())
+
+# Convert to Fahrenheit
+fahrenheit = (celsius * 9/5) + 32
+
+# Print the result
+print(fahrenheit)
+
+## output
+PS C:\Users\hp\OneDrive\Desktop\python_coding_problems> python temperature_conversion.py
+25
+77.0
