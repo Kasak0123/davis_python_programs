c6ac2732e42831efcb395d2d693ed56eec6b4076
Kasak0123 <kasak.yadav20@gmail.com>
2026-02-24 18:37:52 +0530

    Create reverse_temperature_conversion.py

---
+++ reverse_temperature_conversion.py
@@ -0,0 +1,15 @@
+# fahrenheit_to_celsius.py
+
+# Read input from standard input
+fahrenheit = float(input())
+
+# Convert to Celsius
+celsius = (fahrenheit - 32) * 5/9
+
+# Print the result
+print(celsius)
+
+## output
+PS C:\Users\hp\OneDrive\Desktop\python_coding_problems> python reverse_temperature_conversion.py
+77
+25.0
