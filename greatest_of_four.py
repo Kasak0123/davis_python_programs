713d61f32ed42851bfd7ea3350a5f5f3498fc757
Kasak0123 <kasak.yadav20@gmail.com>
2026-02-24 19:42:13 +0530

    Create greatest_of_four.py

---
+++ greatest_of_four.py
@@ -0,0 +1,13 @@
+# Read input from standard input
+a, b, c, d = map(int, input().split())
+
+# Find the greatest number
+greatest = max(a, b, c, d)
+
+# Print the result
+print(greatest)
+
+## output
+PS C:\Users\hp\OneDrive\Desktop\python_coding_problems> python greatest_of_four.py
+12 45 23 9
+45
