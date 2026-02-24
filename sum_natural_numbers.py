1ae93d3456f7363a60e988f8d1735923315cffbb
Kasak0123 <kasak.yadav20@gmail.com>
2026-02-24 11:59:15 +0530

    Create sum_natural_numbers.py

---
+++ sum_natural_numbers.py
@@ -0,0 +1,14 @@
+# sum_natural_numbers.py
+
+# Read input from standard input
+N = int(input())
+
+# Method 1: Using formula
+sum_n = N * (N + 1) // 2
+
+# Print the result
+print(sum_n)
+## output
+PS C:\Users\hp\OneDrive\Desktop\python_coding_problems> python sum_natural_numbers.py
+10
+55
