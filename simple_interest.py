a3826cc2f06dc66093b5a93537b10d5238a5beb5
Kasak0123 <kasak.yadav20@gmail.com>
2026-02-24 18:31:10 +0530

    Create simple_interest.py

---
+++ simple_interest.py
@@ -0,0 +1,13 @@
+# Read input from standard input
+P, R, T = map(float, input().split())
+
+# Calculate Simple Interest
+SI = (P * R * T) / 100
+
+# Print the result
+print(SI)
+
+## output
+PS C:\Users\hp\OneDrive\Desktop\python_coding_problems> python simple_interest.py
+1000 5 2
+100.0
