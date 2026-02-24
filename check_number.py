f58a1a195e7fad73d6935b31810ad064838ee790
Kasak0123 <kasak.yadav20@gmail.com>
2026-02-24 18:25:43 +0530

    Create check_number.py

---
+++ check_number.py
@@ -0,0 +1,15 @@
+# Read input from standard input
+num = int(input())
+
+# Check conditions
+if num > 0:
+    print("Positive")
+elif num < 0:
+    print("Negative")
+else:
+    print("Zero")
+
+## output
+PS C:\Users\hp\OneDrive\Desktop\python_coding_problems> python check_number.py
+10
+Positive
