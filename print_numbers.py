7b811c88c95ebd09efc16f3cedb1157e3945f5cc
Kasak0123 <kasak.yadav20@gmail.com>
2026-02-24 19:47:04 +0530

    Create print_numbers.py

---
+++ print_numbers.py
@@ -0,0 +1,21 @@
+# File name: print_numbers.py
+
+# Read input from standard input
+N = int(input())
+
+# Initialize counter
+i = 1
+
+# Use while loop to print numbers from 1 to N
+while i <= N:
+    print(i)
+    i += 1
+
+## output
+PS C:\Users\hp\OneDrive\Desktop\python_coding_problems> python print_numbers.py
+5
+1
+2
+3
+4
+5
